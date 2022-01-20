36086a0251ca783b70520f40047e4a1b2e2ab1c3
Weverton Alves Guimarães <62063063+WevertonAG@users.noreply.github.com>
2022-01-20 18:27:32 -0300

    Update gerador.js

--- js/gerador.js
+++ js/gerador.js
@@ -1,7 +1,28 @@
-function randomFilme(){
-    const numero = getElementByid('button')
+function animar (){
+ // aqui vai animar algo    
 }
 
-function animar ("id"){
- console.log("passei aqui")
+
+const Filmes = []
+const Comidas = []
+const Bebidas = []
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function gerarFilme(id){
+    
+    console.log(getRandomIntInclusive(1,15),'aqui vai gerar filmes')
+}
+
+function gerarComida(id){
+    console.log(getRandomIntInclusive(1,6),'aqui vai gerar comida ')
+}
+
+function gerarBebida(id){
+    console.log(getRandomIntInclusive(1,3),'Aqui vai gerar bebida')
 }
