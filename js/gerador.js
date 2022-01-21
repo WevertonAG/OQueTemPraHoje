53a150e3ac09d08554140aa040799934a1f3657e
Weverton Alves Guimarães <62063063+WevertonAG@users.noreply.github.com>
2022-01-21 17:23:03 -0300

    Update gerador.js

--- js/gerador.js
+++ js/gerador.js
@@ -1,11 +1,42 @@
-function animar (){
- // aqui vai animar algo    
-}
+const filmes = [
+    "Harry Potter e a Pedra Filosofal",
+    "Harry Potter e a Câmara Secreta",
+    "Harry Potter e o Prisioneiro de Azkaban",
+    "Harry Potter e o Cálice de Fogo",
+    "Harry Potter e a Ordem da Fenix",
+    "Harry Potter e o Enigma do Principe",
+    "Harry Potter e as Relíquias da Morte Parte 1",
+    "Harry Potter e as Relíquias da Morte Parte 2",
+    "Homen Aranha de Volta ao Lar",
+    "Homen Aranha Longe de casa",
+    "Homen Aranha Sem volta pra casa",
+    "Divergente",
+    "Convergente",
+    "Animais Fantasticos e onde habitam",
+    "Animais Fantasticos e os crimes de Grindelwald",
+    "Thor",
+    "Homen de Ferro",
+    "Crepusculo",
+    "Lua nova",
+    "Eclipse",
+    "Amanhecer"
+]
+const comidas = [
+    "Pizza",
+    "Lasanha",
+    "Sanduiche",
+    "Brigadeiro",
+    "Sushi",
+    "Torta"
+]
+const bebidas = [
+    "Agua com Gás",
+    "Suco",
+    "Limonada",
+    "Vinho",
+    "Refrigerante",
 
-
-const Filmes = []
-const Comidas = []
-const Bebidas = []
+]
 
 
 function getRandomIntInclusive(min, max) {
@@ -15,14 +46,19 @@ function getRandomIntInclusive(min, max) {
 }
 
 function gerarFilme(id){
-    
-    console.log(getRandomIntInclusive(1,15),'aqui vai gerar filmes')
+    const filme = filmes[getRandomIntInclusive(0,filmes.length)]
+    const movie = document.getElementById('movie')
+    movie.innerHTML= filme
 }
 
 function gerarComida(id){
-    console.log(getRandomIntInclusive(1,6),'aqui vai gerar comida ')
+    const comida = comidas[getRandomIntInclusive(0,comidas.length)]
+    const food = document.getElementById('food')
+    food.innerHTML= comida
 }
 
 function gerarBebida(id){
-    console.log(getRandomIntInclusive(1,3),'Aqui vai gerar bebida')
+    const bebida = bebidas[getRandomIntInclusive(0,bebidas.length)]
+    const drink = document.getElementById('drink')
+    drink.innerHTML= bebida
 }
